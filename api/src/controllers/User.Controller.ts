d2246775cfb498ea4a5a83b1c70a8b6bda0738a2
bishri <bishri60@gmail.com>
2025-01-23 14:15:42 +0300

    refactor user controller; comment out unused code and improve readability

--- api/src/controllers/User.Controller.ts
+++ api/src/controllers/User.Controller.ts
@@ -2,7 +2,6 @@ import { Request, RequestHandler, Response } from "express-serve-static-core";
 import {
   authService,
   clinicProfileService,
-  // clinicService,
   permissionService,
   userService,
 } from "../services";
@@ -19,9 +18,9 @@ import {
 import { generateAccessToken, generateRefreshToken } from "../utils/token";
 import asyncHandler from "../utils/asyncHandler";
 
-const db = require("../models");
+// const db = require("../models");
 // import { generateToken } from "../utils/generateToken"
-import { Op } from "sequelize";
+// import { Op } from "sequelize";
 import { ApiError } from "../shared/error/ApiError";
 
 export const getUsers = asyncHandler(async (req: Request, res: Response) => {
@@ -31,37 +30,22 @@ export const getUsers = asyncHandler(async (req: Request, res: Response) => {
 });
 export const getActiveUsers: RequestHandler = asyncHandler(
   async (req: Request, res: Response) => {
-    const users = await User.findAll({
-      where: { status: true },
-      attributes: { exclude: ["password"] },
-      include: [
-        {
-          model: db.Employee,
-          as: "employee",
-
-          attributes: ["id", "firstName", "lastName", "middleName"],
-        },
-      ],
-    });
-    res.json(users);
+    // const users = await User.findAll({
+    //   where: { status: true },
+    //   attributes: { exclude: ["password"] },
+    //   include: [
+    //     {
+    //       model: Employee,
+    //       as: "employee",
+    //       attributes: ["id", "firstName", "lastName", "middleName"],
+    //     },
+    //   ],
+    // });
+    // res.json(users);
   }
 );
 export const getDoctors = asyncHandler(async (req: Request, res: Response) => {
-  // const { query } = req.query;
-  // console.log(req.user);
-  // const role = await db.Role.findOne({
-  //   where: {
-  //     name: {
-  //       [Op.like]: `%doctor%`,
-  //     },
-  //   },
-  // });
   const doctors = await userService.getDoctorsUser();
-  // User.findAll({
-  //   where: { role_id: role.id, status: true },
-  //   attributes: { exclude: ["password"] },
-  //   include: ["employee", "schedules", "role"],
-  // });
   res.json(doctors);
 });
 export const getUserById = asyncHandler(
@@ -69,7 +53,7 @@ export const getUserById = asyncHandler(
     const userId = parseInt(req.params.id, 10);
     const user = await userService.getUserById(userId);
     const { password, ...userData } = user.toJSON();
-    console.log(userData);
+
     res.json({
       success: true,
       data: userData,
@@ -171,58 +155,22 @@ export const activateUser = asyncHandler(
       message: "Account successfully activated",
       data: userData,
     });
-    // console.log(updatedUser);
   }
 );
 export const addWorkingHour = asyncHandler<{
   validatedData: addDoctorWorkingHoursInput;
 }>(async (req, res: Response) => {
-  // const { id } = req.params;
-  // const { start_time, end_time, doctorId, day_of_week } = req.body;
-  // console.log(req.body);
-  // console.log(resq.validatedData);
-
   const doctorId = parseInt(req.params.id, 10);
   const schedule = await userService.AddDoctorWorkingHour(
     doctorId,
     req.validatedData
   );
-  // const clinicISworking = await db.Schedule.findOne({
-  //   clinis_id: 4,
-  //   // doctor_id: doctorId,
-  //   day_of_week: day_of_week,
-  // });
-  // // i want to check if clinic is open or not
-  // if (clinicISworking) {
-  //   if (
-  //     clinicISworking.start_time > start_time ||
-  //     clinicISworking.end_time < end_time
-  //   ) {
-  //     res.status(400);
-  //     throw new Error("Sorry, the clinic is not open at this time");
-  //   }
-  // } else {
-  //   res.status(404);
-  //   throw new Error("Sorry, the clinic is not open on this day.");
-  // }
-  // const schedule = await db.Schedule.create(
-  //   {
-  //     start_time: start_time,
-  //     end_time: end_time,
-  //     doctor_id: doctorId,
-  //     day_of_week: day_of_week,
-  //   },
-  //   { userId: req?.user?.id }
-  // );
+
   res.status(201).json({ success: true, message: "success", data: schedule });
 });
 export const updateWorkHour = asyncHandler<{
   validatedData: addDoctorWorkingHoursInput;
 }>(async (req, res: Response) => {
-  const { id } = req.params;
-  // const { start_time, end_time, doctorId, day_of_week } = req.body;
-  // console.log(req.body);
-  // console.log(id);
   const doctorId = parseInt(req.params.id, 10);
   const scheduleId = parseInt(req.params.scheduleId, 10);
   const schedule = await userService.updatedDoctorWorkingHour(
@@ -230,31 +178,7 @@ export const updateWorkHour = asyncHandler<{
     scheduleId,
     req.validatedData
   );
-  //   const schedule = await db.Schedule.findByPk(id);
-  //   if (!schedule) {
-  //     res.status(404);
-  //     throw new Error("Schedule not found");
-  //   }
-  //   const clinicISworking = await db.Schedule.findOne({
-  //     clinis_id: 4,
-  //     // doctor_id: doctorId,
-  //     day_of_week: day_of_week,
-  //   });
-  //   // i want to check if clinic is open or not
-  //   if (clinicISworking) {
-  //     if (
-  //       clinicISworking.start_time > start_time ||
-  //       clinicISworking.end_time < end_time
-  //     ) {
-  //       res.status(400);
-  //       throw new Error("Sorry, the clinic is not open at this time range");
-  //     }
-  //   }
-  //   schedule.start_time = start_time;
-  //   schedule.end_time = end_time;
-  //   schedule.doctor_id = doctorId;
-  //   schedule.day_of_week = day_of_week;
-  //   await schedule.save({ userId: req?.user?.id });
+
   res.json({
     success: true,
     message: "schedule successfully updated",
@@ -318,18 +242,18 @@ export const changePassword = asyncHandler<{
 });
 export const resetPassword = asyncHandler(
   async (req: Request, res: Response) => {
-    const username = "user-0003";
-    const user = await db.User.findOne({
-      where: {
-        username: username,
-      },
-    });
-    if (!user) {
-      res.status(404);
-      throw new Error("User not found");
-    }
-    user.password = "123456";
-    await user.save();
+    // const username = "user-0003";
+    // const user = await db.User.findOne({
+    //   where: {
+    //     username: username,
+    //   },
+    // });
+    // if (!user) {
+    //   res.status(404);
+    //   throw new Error("User not found");
+    // }
+    // user.password = "123456";
+    // await user.save();
     res.status(200).json({ msg: "success" });
   }
 );
