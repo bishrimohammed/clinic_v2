d1161acb25aa0285766973b3de0e662fe1162c70
bishri <bishri60@gmail.com>
2025-01-31 19:11:07 +0300

    feat: implement patient visit retrieval with search and sorting capabilities

--- api/src/services/visit.service.ts
+++ api/src/services/visit.service.ts
@@ -1,1 +1,270 @@
+import { Op, Sequelize } from "sequelize";
+import { patientVisitQueryType } from "../types/visit";
+import { Employee, Patient, PatientVisit, User } from "../models";
+import { userService } from ".";
+import { ApiError } from "../shared/error/ApiError";
 
+export const getVisits = async (query: patientVisitQueryType) => {
+  const { q: searchTerm, sortBy } = query;
+  const page = parseInt(query.page) || 1;
+  const limit = parseInt(query.limit) || 10;
+  const whereClause: any = {};
+  const orderClause: any = [];
+
+  // search by patient name, patient id,
+  if (searchTerm) {
+    whereClause[Op.or] = [
+      {
+        [Op.or]: [
+          { "$patient.firstName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.lastName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.middleName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.card_number$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.phone$": { [Op.like]: `%${searchTerm}%` } },
+        ],
+      },
+      Sequelize.literal(
+        `CONCAT(patient.firstName, ' ', patient.middleName, ' ', patient.lastName) LIKE '%${searchTerm}%'`
+      ),
+      Sequelize.literal(
+        `CONCAT(patient.firstName, ' ', patient.middleName) LIKE '%${searchTerm}%'`
+      ),
+    ];
+  }
+
+  switch (sortBy) {
+    case "patientName_asc":
+      orderClause.push(
+        ["patient", "firstName", "ASC"],
+        ["patient", "lastName", "ASC"],
+        ["patient", "middleName", "ASC"]
+      );
+      break;
+    case "patientName_desc":
+      orderClause.push(
+        ["patient", "firstName", "DESC"],
+        ["patient", "lastName", "DESC"],
+        ["patient", "middleName", "DESC"]
+      );
+      break;
+    case "patientId_asc":
+      orderClause.push(["patient", "card_number", "ASC"]);
+      break;
+    case "patientId_desc":
+      orderClause.push(["patient", "card_number", "DESC"]);
+      break;
+    // case "doctorName_asc":
+    //   orderClause.push(["gender", "ASC"]);
+    //   break;
+    // case "sex_desc":
+    //   orderClause.push(["gender", "DESC"]);
+    //   break;
+    case "visit_date_asc":
+      orderClause.push(["visitDate", "ASC"], ["visitTime", "ASC"]);
+      break;
+
+    case "visit_date_desc":
+      orderClause.push(["visitDate", "DESC"], ["visitTime", "DESC"]);
+      break;
+    default: {
+      orderClause.push(["visitDate", "DESC"], ["visitTime", "DESC"]);
+    }
+  }
+  const offset = (page - 1) * limit;
+  const { rows: visits, count } = await PatientVisit.findAndCountAll({
+    where: whereClause,
+    include: [
+      {
+        model: Patient,
+        as: "patient",
+        attributes: [
+          "id",
+          "firstName",
+          "lastName",
+          "middleName",
+          "card_number",
+          "patient_type",
+        ],
+      },
+      {
+        model: User,
+        as: "doctor",
+        include: [
+          {
+            model: Employee,
+            as: "employee",
+            attributes: ["id", "firstName", "middleName", "lastName"],
+          },
+        ],
+        attributes: ["id"],
+      },
+    ],
+    order: orderClause,
+    offset,
+    limit,
+  });
+  const hasMore = count > page * limit;
+  return {
+    visits,
+    pagination: {
+      currentPage: page,
+      totalResults: count,
+      totalPages: Math.ceil(count / limit),
+      hasMore,
+    },
+  };
+};
+
+export const getActiveVisits = async (
+  query: patientVisitQueryType,
+  userId: number
+) => {
+  const { q: searchTerm, sortBy } = query;
+  const page = parseInt(query.page) || 1;
+  const limit = parseInt(query.limit) || 10;
+  const whereClause: any = {};
+  const orderClause: any = [];
+
+  // search by patient name, patient id,
+  if (searchTerm) {
+    whereClause[Op.or] = [
+      {
+        [Op.or]: [
+          { "$patient.firstName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.lastName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.middleName$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.card_number$": { [Op.like]: `%${searchTerm}%` } },
+          { "$patient.phone$": { [Op.like]: `%${searchTerm}%` } },
+        ],
+      },
+      Sequelize.literal(
+        `CONCAT(patient.firstName, ' ', patient.middleName, ' ', patient.lastName) LIKE '%${searchTerm}%'`
+      ),
+      Sequelize.literal(
+        `CONCAT(patient.firstName, ' ', patient.middleName) LIKE '%${searchTerm}%'`
+      ),
+    ];
+  }
+  const user = await userService.getUserById(userId);
+  if (await user.isDoctorRole()) {
+    whereClause["doctorId"] = userId;
+  }
+  whereClause["status"] = true;
+
+  switch (sortBy) {
+    case "patientName_asc":
+      orderClause.push(
+        ["patient", "firstName", "ASC"],
+        ["patient", "lastName", "ASC"],
+        ["patient", "middleName", "ASC"]
+      );
+      break;
+    case "patientName_desc":
+      orderClause.push(
+        ["patient", "firstName", "DESC"],
+        ["patient", "lastName", "DESC"],
+        ["patient", "middleName", "DESC"]
+      );
+      break;
+    case "patientId_asc":
+      orderClause.push(["patient", "card_number", "ASC"]);
+      break;
+    case "patientId_desc":
+      orderClause.push(["patient", "card_number", "DESC"]);
+      break;
+    // case "doctorName_asc":
+    //   orderClause.push(["gender", "ASC"]);
+    //   break;
+    // case "sex_desc":
+    //   orderClause.push(["gender", "DESC"]);
+    //   break;
+    case "visit_date_asc":
+      orderClause.push(["visitDate", "ASC"], ["visitTime", "ASC"]);
+      break;
+
+    case "visit_date_desc":
+      orderClause.push(["visitDate", "DESC"], ["visitTime", "DESC"]);
+      break;
+    default: {
+      orderClause.push(["visitDate", "DESC"], ["visitTime", "DESC"]);
+    }
+  }
+  const offset = (page - 1) * limit;
+  const { rows: visits, count } = await PatientVisit.findAndCountAll({
+    where: whereClause,
+    include: [
+      {
+        model: Patient,
+        as: "patient",
+        attributes: [
+          "id",
+          "firstName",
+          "lastName",
+          "middleName",
+          "card_number",
+          "patient_type",
+        ],
+      },
+      {
+        model: User,
+        as: "doctor",
+        include: [
+          {
+            model: Employee,
+            as: "employee",
+            attributes: ["id", "firstName", "middleName", "lastName"],
+          },
+        ],
+        attributes: ["id"],
+      },
+    ],
+    order: orderClause,
+    offset,
+    limit,
+  });
+  const hasMore = count > page * limit;
+  return {
+    visits,
+    pagination: {
+      currentPage: page,
+      totalResults: count,
+      totalPages: Math.ceil(count / limit),
+      hasMore,
+    },
+  };
+};
+
+export const getPatientVisitById = async (id: number) => {
+  const visit = await PatientVisit.findByPk(id, {
+    include: [
+      {
+        model: Patient,
+        as: "patient",
+        attributes: [
+          "id",
+          "firstName",
+          "lastName",
+          "middleName",
+          "card_number",
+          "patient_type",
+        ],
+      },
+      {
+        model: User,
+        as: "doctor",
+        include: [
+          {
+            model: Employee,
+            as: "employee",
+            attributes: ["id", "firstName", "middleName", "lastName"],
+          },
+        ],
+        attributes: ["id"],
+      },
+    ],
+  });
+  if (!visit) {
+    throw new ApiError(404, "patientVisit not found");
+  }
+  return visit;
+};
