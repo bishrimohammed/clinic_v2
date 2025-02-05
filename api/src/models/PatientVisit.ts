192cf7eda5f90d88de98ce60ed2f8519e1f45991
bishri <bishri60@gmail.com>
2025-02-05 12:50:16 +0300

    feat: update PatientVisit model to include referredTo field and make visitType and status optional; add getRegistrationFeeServiceItem function in clinic service

--- api/src/models/PatientVisit.ts
+++ api/src/models/PatientVisit.ts
@@ -18,12 +18,13 @@ class PatientVisit extends Model<
   declare medicalRecordId: string;
   // declare visitType_id?: number | null;
   declare id: CreationOptional<number>;
-  // declare patientId: number;
+  declare patientId: number;
   declare doctorId: number;
+  declare referredTo: number | null;
   declare visitDate: Date;
   declare visitTime: string; // Format: HH:mm:ss
-  declare visitType?: string | null;
-  declare isReferred: boolean;
+  declare visitType: string;
+  declare isReferred?: boolean;
   declare reason?: string | null;
   declare createdBy: number;
   declare symptomNotes?: string | null;
@@ -38,14 +39,14 @@ class PatientVisit extends Model<
     | "Waiting for payment"
     | "Waiting for lab"
     | "Waiting for doctor"
-    | "Done"
-    | null;
+    | "Done";
+
   declare isAdmitted?: boolean;
   declare admissionDate?: Date | null;
   declare dischargeSummary?: string | null;
   declare dischargedBy?: number | null;
   declare dischargedDate?: Date | null;
-  declare status: boolean;
+  declare status?: boolean;
   declare createdAt?: CreationOptional<Date>;
   declare updatedAt?: CreationOptional<Date>;
 }
@@ -58,13 +59,21 @@ PatientVisit.init(
       autoIncrement: true,
       allowNull: false,
     },
-    // patientId: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    // },
+    patientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     doctorId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+    },
+    referredTo: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // references: {
+      //   model: User,
+      //   key: "id",
+      // },
     },
     medicalRecordId: {
       type: DataTypes.UUID,
@@ -117,12 +126,12 @@ PatientVisit.init(
         "Waiting for service fee",
         "Waiting for triage",
         "Waiting for examiner",
+        "Waiting for doctor",
         "Performing triage",
         "Admitted",
         "Performing consultation",
         "Waiting for payment",
         "Waiting for lab",
-        "Waiting for doctor",
         "Done"
       ),
       allowNull: true,
@@ -166,102 +175,21 @@ PatientVisit.init(
     sequelize,
     tableName: "patient_visits",
     timestamps: true,
-    // hooks: {
-    //   afterCreate: async (visit, options) => {
-    //     await sequelize.models.patientvisits_audit.create({
-    //       patient_visit_id: visit.id,
-    //       doctor_id: visit.doctor_id,
-    //       patient_id: visit.patient_id,
-    //       medicalRecord_id: visit.medicalRecord_id,
-    //       visit_type_id: visit.visitType_id,
-    //       assignment_date: visit.assignment_date,
-    //       visit_time: visit.visit_time,
-    //       visit_type: visit.visit_type,
-    //       is_referred: visit.is_referred,
-    //       reason: visit.reason,
-    //       created_by: visit.created_by,
-    //       symptom_notes: visit.symptom_notes,
-    //       mode_of_arrival: visit.mode_of_arrival,
-    //       stage: visit.stage,
-    //       status: visit.status,
-    //       isAdmitted: visit.isAdmitted,
-    //       admission_date: visit.admission_date,
-    //       discharge_summary: visit.discharge_summary,
-    //       discharged_by: visit.discharged_by,
-    //       discharged_date: visit.discharged_date,
-    //       operation_type: "I",
-    //       changed_by: options.userId,
-    //       changed_at: Date.now(),
-    //     });
-    //   },
-    //   beforeUpdate: async (visit, options) => {
-    //     const previousValue = visit._previousDataValues;
-    //     await sequelize.models.patientvisits_audit.create({
-    //       patient_visit_id: previousValue.id,
-    //       doctor_id: previousValue.doctor_id,
-    //       patient_id: previousValue.patient_id,
-    //       medicalRecord_id: previousValue.medicalRecord_id,
-    //       visit_type_id: previousValue.visitType_id,
-    //       assignment_date: previousValue.assignment_date,
-    //       visit_time: previousValue.visit_time,
-    //       visit_type: previousValue.visit_type,
-    //       is_referred: previousValue.is_referred,
-    //       reason: previousValue.reason,
-    //       created_by: previousValue.created_by,
-    //       symptom_notes: previousValue.symptom_notes,
-    //       mode_of_arrival: previousValue.mode_of_arrival,
-    //       stage: previousValue.stage,
-    //       status: previousValue.status,
-    //       isAdmitted: previousValue.isAdmitted,
-    //       admission_date: previousValue.admission_date,
-    //       discharge_summary: previousValue.discharge_summary,
-    //       discharged_by: previousValue.discharged_by,
-    //       discharged_date: previousValue.discharged_date,
-    //       operation_type: "U",
-    //       changed_by: options.userId,
-    //       changed_at: Date.now(),
-    //     });
-    //   },
-    //   beforeDestroy: async (visit, options) => {
-    //     await sequelize.models.patientvisits_audit.create({
-    //       patient_visit_id: visit.id,
-    //       doctor_id: visit.doctor_id,
-    //       patient_id: visit.patient_id,
-    //       medicalRecord_id: visit.medicalRecord_id,
-    //       visit_type_id: visit.visitType_id,
-    //       assignment_date: visit.assignment_date,
-    //       visit_time: visit.visit_time,
-    //       visit_type: visit.visit_type,
-    //       is_referred: visit.is_referred,
-    //       reason: visit.reason,
-    //       created_by: visit.created_by,
-    //       symptom_notes: visit.symptom_notes,
-    //       mode_of_arrival: visit.mode_of_arrival,
-    //       stage: visit.stage,
-    //       status: visit.status,
-    //       isAdmitted: visit.isAdmitted,
-    //       admission_date: visit.admission_date,
-    //       discharge_summary: visit.discharge_summary,
-    //       discharged_by: visit.discharged_by,
-    //       discharged_date: visit.discharged_date,
-    //       operation_type: "D",
-    //       changed_by: options.userId,
-    //       changed_at: Date.now(),
-    //     });
-    //   },
-    // },
   }
 );
 
-// PatientVisit.belongsTo(Patient, {
-//   foreignKey: "patientId",
-//   as: "patient",
-// });
+PatientVisit.belongsTo(Patient, {
+  foreignKey: "patientId",
+  as: "patient",
+});
 PatientVisit.belongsTo(User, {
   foreignKey: "doctorId",
   as: "doctor",
 });
-
-// PatientVisit.sync({ force: false, alter: false });
+PatientVisit.belongsTo(User, {
+  foreignKey: "refferedTo",
+  as: "refferedto",
+});
+PatientVisit.sync({ force: false, alter: true });
 
 export default PatientVisit;
