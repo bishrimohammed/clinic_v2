297abd756445b6e53c46e6452729d131b1f3a045
bishri <bishri60@gmail.com>
2025-02-06 23:55:19 +0300

    feat: enhance medical record service with new methods for retrieving and adding vital signs and symptoms

--- api/src/services/medicalrecord.service.ts
+++ api/src/services/medicalrecord.service.ts
@@ -1,7 +1,21 @@
 import { Transaction } from "sequelize";
 import MedicalRecord from "../models/MedicalRecord";
-
-export const parientActiveMedicalRecord = async (patientId: number) => {
+import {
+  addMedicalRecordSymptomsType,
+  addVitalSignType,
+} from "../types/medical-record";
+import { loggedInUserId } from "../types/shared";
+import { ApiError } from "../shared/error/ApiError";
+import { PatientVisit, VitalSign, VitalSignResult } from "../models";
+import sequelize from "../db";
+/**
+ * Get patient active medical record
+ * @param patientId - patient id
+ * @returns {MedicalRecord | null}
+ */
+export const parientActiveMedicalRecord = async (
+  patientId: number
+): Promise<MedicalRecord | null> => {
   const record = await MedicalRecord.findOne({
     where: {
       patientId,
@@ -10,8 +24,14 @@ export const parientActiveMedicalRecord = async (patientId: number) => {
   });
   return record;
 };
-
-export const getPatientLastMedicalRecord = async (patientId: number) => {
+/**
+ * Get patient last medical record
+ * @param patientId - patient id
+ * @returns {Promise<MedicalRecord| null>}
+ */
+export const getPatientLastMedicalRecord = async (
+  patientId: number
+): Promise<MedicalRecord | null> => {
   const record = await MedicalRecord.findOne({
     where: {
       patientId,
@@ -20,10 +40,28 @@ export const getPatientLastMedicalRecord = async (patientId: number) => {
   });
   return record;
 };
+/**
+ * Get medical record by id
+ * @param medicalRecordId - medical record id
+ * @returns
+ */
+export const getMedicalRecordById = async (medicalRecordId: string) => {
+  const medicalRecord = await MedicalRecord.findByPk(medicalRecordId);
+  if (!medicalRecord) {
+    throw new ApiError(404, "medical record not found");
+  }
+  return medicalRecord;
+};
+/**
+ *
+ * @param patientId - patient id
+ * @param transaction -
+ * @returns {Promise<MedicalRecord>}
+ */
 export const createMedicalRecord = async (
   patientId: number,
   transaction?: Transaction
-) => {
+): Promise<MedicalRecord> => {
   const record = await MedicalRecord.create(
     {
       patientId,
@@ -31,6 +69,61 @@ export const createMedicalRecord = async (
     { transaction }
   );
   return record;
+};
+/**
+ * Add vital signs
+ * @param medicalRecordId - medical record id
+ * @param vitalSigns - vital signs
+ * @param userId - logged in user id
+ * @param transaction - transaction
+ * @returns
+ */
+export const addVitalSigns = async (
+  medicalRecordId: string,
+  vitalSigns: addVitalSignType,
+  userId: loggedInUserId,
+  transaction?: Transaction
+) => {
+  // const medicalRecord = await getMedicalRecordById(medicalRecordId);
+  // const visit = await PatientVisit.findOne({
+  //   where:{medicalRecordId:medicalRecordId, doctorId:userId}
+  // })
+  // const transaction = await sequelize.transaction();
+  // try {
+  const createdVitalSigns = await VitalSign.create(
+    {
+      medicalRecordId,
+      examinerId: userId,
+    },
+    { transaction }
+  );
+
+  const transformVitalSigns = vitalSigns.map((vitalSign) => ({
+    result: vitalSign.value || "",
+    vitalSignFieldId: vitalSign.vitalSignFieldId,
+    vitalId: createdVitalSigns.id,
+  }));
+  await VitalSignResult.bulkCreate(transformVitalSigns, { transaction });
+  await createdVitalSigns.getVitalResults();
+  // await transaction.commit();
+  return createdVitalSigns;
+  // } catch (error) {
+  //   await transaction.rollback();
+  //   throw error;
+  // }
+};
+/**
+ * Add medical record symptoms
+ * @param medicalRecordId -
+ * @param data - Medical record symptoms data
+ * @param userId - logged in doctor id
+ */
+export const addMedicalRecordSymptoms = async (
+  medicalRecordId: string,
+  data: addMedicalRecordSymptomsType,
+  userId: loggedInUserId
+) => {
+  const { HPI, chiefComplaint } = data;
 };
 // export const createMedicalRecord = async (
 //   data: createMedicalRecordType,
