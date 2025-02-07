ca590a3a2eee2a436b27f781835db782d13b28f2
bishri <bishri60@gmail.com>
2025-02-07 13:15:13 +0300

    feat: add methods for retrieving and updating medical record details and symptoms

--- api/src/services/medicalrecord.service.ts
+++ api/src/services/medicalrecord.service.ts
@@ -6,8 +6,16 @@ import {
 } from "../types/medical-record";
 import { loggedInUserId } from "../types/shared";
 import { ApiError } from "../shared/error/ApiError";
-import { PatientVisit, VitalSign, VitalSignResult } from "../models";
+import {
+  MedicalRecordDetail,
+  PatientVisit,
+  VitalSign,
+  VitalSignField,
+  VitalSignResult,
+} from "../models";
 import sequelize from "../db";
+import { checkPatientMedicalRecordAssignedDoctorToVisit } from "./visit.service";
+
 /**
  * Get patient active medical record
  * @param patientId - patient id
@@ -53,7 +61,7 @@ export const getMedicalRecordById = async (medicalRecordId: string) => {
   return medicalRecord;
 };
 /**
- *
+ * Create medical record
  * @param patientId - patient id
  * @param transaction -
  * @returns {Promise<MedicalRecord>}
@@ -70,48 +78,23 @@ export const createMedicalRecord = async (
   );
   return record;
 };
-/**
- * Add vital signs
- * @param medicalRecordId - medical record id
- * @param vitalSigns - vital signs
- * @param userId - logged in user id
- * @param transaction - transaction
- * @returns
- */
-export const addVitalSigns = async (
-  medicalRecordId: string,
-  vitalSigns: addVitalSignType,
-  userId: loggedInUserId,
-  transaction?: Transaction
-) => {
-  // const medicalRecord = await getMedicalRecordById(medicalRecordId);
-  // const visit = await PatientVisit.findOne({
-  //   where:{medicalRecordId:medicalRecordId, doctorId:userId}
-  // })
-  // const transaction = await sequelize.transaction();
-  // try {
-  const createdVitalSigns = await VitalSign.create(
-    {
-      medicalRecordId,
-      examinerId: userId,
-    },
-    { transaction }
-  );
 
-  const transformVitalSigns = vitalSigns.map((vitalSign) => ({
-    result: vitalSign.value || "",
-    vitalSignFieldId: vitalSign.vitalSignFieldId,
-    vitalId: createdVitalSigns.id,
-  }));
-  await VitalSignResult.bulkCreate(transformVitalSigns, { transaction });
-  await createdVitalSigns.getVitalResults();
-  // await transaction.commit();
-  return createdVitalSigns;
-  // } catch (error) {
-  //   await transaction.rollback();
-  //   throw error;
-  // }
+//#region Medical Record Symptoms
+/**
+ * Get  medical record detail by id
+ * @param id - medical record detail id
+ * @returns {Promise<MedicalRecordDetail>}
+ */
+export const getMedicalRecordDetailById = async (
+  id: string
+): Promise<MedicalRecordDetail> => {
+  const medicalRecordDetail = await MedicalRecordDetail.findByPk(id);
+  if (!medicalRecordDetail) {
+    throw new ApiError(404, "Medical Record Detail not found");
+  }
+  return medicalRecordDetail;
 };
+
 /**
  * Add medical record symptoms
  * @param medicalRecordId -
@@ -124,7 +107,207 @@ export const addMedicalRecordSymptoms = async (
   userId: loggedInUserId
 ) => {
   const { HPI, chiefComplaint } = data;
+  const visit = await checkPatientMedicalRecordAssignedDoctorToVisit(
+    medicalRecordId,
+    userId
+  );
+  if (!visit) {
+    throw new ApiError(400, "This medical record is not assigned to you");
+  }
+
+  const existingmedicalRecordDetail = await MedicalRecordDetail.findOne({
+    where: {
+      medicalRecordId,
+      doctorId: userId,
+    },
+  });
+
+  const stringifiedChiefComplaint = JSON.stringify(chiefComplaint);
+  // if exist update it
+  if (existingmedicalRecordDetail) {
+    await existingmedicalRecordDetail.update({
+      chiefComplaint:
+        stringifiedChiefComplaint || existingmedicalRecordDetail.chiefComplaint,
+      HPI: HPI || existingmedicalRecordDetail.HPI,
+    });
+    return existingmedicalRecordDetail;
+  }
+
+  const medicalRecordDetail = await MedicalRecordDetail.create({
+    medicalRecordId,
+    doctorId: userId,
+    chiefComplaint: stringifiedChiefComplaint,
+    HPI,
+  });
+  return medicalRecordDetail;
 };
+/**
+ *
+ * @param medicalRecordDetailId - medical record detail id
+ * @param data - Medical record detail symptoms data
+ * @param userId -  logged in doctor id
+ * @returns {Promise<MedicalRecordDetail>}
+ */
+export const updateMedicalRecordSymptoms = async (
+  medicalRecordDetailId: string,
+  data: addMedicalRecordSymptomsType,
+  userId: loggedInUserId
+): Promise<MedicalRecordDetail> => {
+  const { HPI, chiefComplaint } = data;
+
+  const medicalRecordDetail = await getMedicalRecordDetailById(
+    medicalRecordDetailId
+  );
+  if (userId !== medicalRecordDetail.doctorId) {
+    throw new ApiError(
+      400,
+      "This medical record detail is created by another doctor"
+    );
+  }
+  const stringifiedChiefComplaint = JSON.stringify(chiefComplaint);
+
+  await medicalRecordDetail.update({
+    chiefComplaint:
+      stringifiedChiefComplaint || medicalRecordDetail.chiefComplaint,
+    HPI: HPI || medicalRecordDetail.HPI,
+  });
+  return medicalRecordDetail;
+};
+/**
+ *
+ * @param medicalRecordId - medical record id
+ * @param data - Medical record detail plan data
+ * @param userId - logged in doctor id
+ * @param transaction - transaction
+ * @returns {Promise<MedicalRecordDetail>}
+ */
+export const addPlanAndAssessment = async (
+  medicalRecordId: string,
+  data: { plan: string; assessment: string },
+  userId: loggedInUserId,
+  transaction?: Transaction
+): Promise<MedicalRecordDetail> => {
+  const {
+    // diagnosis,
+    // treatmentPlan,
+    // followUpPlan,
+    // medication,
+    // allergies,
+    // immunizations,
+    // labTests,
+    // radiologyTests,
+    // imagingTests,
+    // procedures,
+    // notes,
+    // status,
+    assessment,
+    plan,
+  } = data;
+  // const visit = await checkPatientMedicalRecordAssignedDoctorToVisit(
+  //   medicalRecordId,
+  //   userId
+  // );
+  // if (!visit) {
+  //   throw new ApiError(400, "This medical record is not assigned to you");
+  // }
+  const medicalRecordDetail = await MedicalRecordDetail.findOne({
+    where: {
+      medicalRecordId,
+      doctorId: userId,
+    },
+  });
+  if (!medicalRecordDetail) {
+    throw new ApiError(404, "Medical Record Detail not found");
+  }
+  await medicalRecordDetail.update(
+    {
+      assessment,
+      plan,
+    },
+    { transaction }
+  );
+  return medicalRecordDetail;
+};
+//#endregion
+//#region Vital Signs
+// export const getVitalSigns = async (
+//   medicalRecordId: string,
+//   userId: loggedInUserId
+// ) => {
+//   // const medicalRecord = await getMedicalRecordById(medicalRecordId);
+//   // const visit = await PatientVisit.findOne({
+//   //   where: {
+//   //     medicalRecordId: medicalRecordId,
+//   //     doctorId: userId,
+//   //   },
+//   // });
+//   // if (!visit) {
+//   //   throw new ApiError(400, "This medical record is not assigned to you");
+//   // }
+//   const vitalSigns = await VitalSign.findAll({
+//     where: {
+//       medicalRecordId,
+//     },
+//     include:[
+//       {
+//         model: VitalSignResult,
+//         as: "vitalResults",
+//         include:[
+//           {
+//             model: VitalSignField,
+//             as: "vitalSignField",
+//             attributes:["id","name"]
+//           }
+//         ]
+//       }
+//     ]
+//   });
+//   return vitalSigns;
+// };
+// /**
+//  * Add vital signs
+//  * @param medicalRecordId - medical record id
+//  * @param vitalSigns - vital signs
+//  * @param userId - logged in user id
+//  * @param transaction - transaction
+//  * @returns
+//  */
+// export const addVitalSigns = async (
+//   medicalRecordId: string,
+//   vitalSigns: addVitalSignType,
+//   userId: loggedInUserId,
+//   transaction?: Transaction
+// ) => {
+//   // const medicalRecord = await getMedicalRecordById(medicalRecordId);
+//   // const visit = await PatientVisit.findOne({
+//   //   where:{medicalRecordId:medicalRecordId, doctorId:userId}
+//   // })
+//   // const transaction = await sequelize.transaction();
+//   // try {
+//   const createdVitalSigns = await VitalSign.create(
+//     {
+//       medicalRecordId,
+//       examinerId: userId,
+//     },
+//     { transaction }
+//   );
+
+//   const transformVitalSigns = vitalSigns.map((vitalSign) => ({
+//     result: vitalSign.value || "",
+//     vitalSignFieldId: vitalSign.vitalSignFieldId,
+//     vitalId: createdVitalSigns.id,
+//   }));
+//   await VitalSignResult.bulkCreate(transformVitalSigns, { transaction });
+//   await createdVitalSigns.getVitalResults();
+//   // await transaction.commit();
+//   return createdVitalSigns;
+//   // } catch (error) {
+//   //   await transaction.rollback();
+//   //   throw error;
+//   // }
+// };
+
+//#endregion
 // export const createMedicalRecord = async (
 //   data: createMedicalRecordType,
 //   userId: number
