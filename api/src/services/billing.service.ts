c0ea29c220364a063aefd30a6cac9eaab33e1172
bishri <bishri60@gmail.com>
2025-07-07 19:43:44 +0300

    feat: implement methods for retrieving billing items and creating invoices from service line items

--- api/src/services/billing.service.ts
+++ api/src/services/billing.service.ts
@@ -19,8 +19,12 @@ export const getMedicalBillingById = async (id: string) => {
 };
 /**
  * Create medical billing
- * @param data
- * @returns
+ * @param billableId
+ * @param billableType
+ * @param transaction
+ * @returns MedicalBilling
+ * @description Creates a new medical billing record if it does not already exist.
+
  */
 export const createMedicalBilling = async (
   billableId: string,
@@ -111,4 +115,94 @@ export const addBulkBillingItemsToMedicalBilling = async (data: {
     })),
     { transaction }
   );
+};
+
+/**
+ * Get billing items by medical billing id
+ * @param medicalBillingId
+ * @returns
+ */
+export const getBillingItemsByMedicalBillingId = async (
+  medicalBillingId: string
+) => {
+  const billingItems = await ServiceLineItem.findAll({
+    where: { billingId: medicalBillingId },
+    include: [
+      {
+        model: MedicalBilling,
+        as: "billing",
+      },
+      {
+        model: ServiceLineItem,
+        as: "serviceItem",
+      },
+    ],
+  });
+  if (!billingItems || billingItems.length === 0) {
+    throw new ApiError(404, "No billing items found for this medical billing");
+  }
+  return billingItems;
+};
+
+/**
+ * Take invoice payment of medical billing from array of service line items
+ * @param medicalBillingId
+ * @param  Array of service line item ids to be paid
+ * @param userId
+ *
+ */
+export const takeInvoicePaymentFromServiceLineItems = async (
+  medicalBillingId: string,
+  items: { serviceItemId: number; price: number }[],
+  paymentMethod: "Cash" | "Card" | "Insurance" | "Mobile",
+  userId: number,
+  transaction?: Transaction
+) => {};
+
+/**
+ * create invoice for medical billing from selected service line items
+ * @param medicalBillingId
+ * @param items
+ */
+
+export const createInvoiceFromServiceLineItems = async (
+  medicalBillingId: string,
+  items: string[],
+  userId: number,
+  transaction?: Transaction
+) => {
+  // Validate medical billing exists
+  const medicalBilling = await getMedicalBillingById(medicalBillingId);
+  // fetch service line items
+  const serviceLineItems = await medicalBilling.getServiceLineItems({
+    where: { id: items },
+    include: [
+      {
+        model: ServiceLineItem,
+        as: "serviceItem",
+        attributes: ["id", "price"],
+      },
+    ],
+    transaction,
+  });
+  if (!serviceLineItems || serviceLineItems.length === 0) {
+    throw new ApiError(
+      404,
+      "No service line items found for this medical billing"
+    );
+  }
+  // check if there is items not in medical billing
+  if (serviceLineItems.length !== items.length) {
+    throw new ApiError(
+      400,
+      "Some service line items are not part of this medical billing"
+    );
+  }
+  // calculate total amount
+  const totalAmount = serviceLineItems.reduce((acc, item) => {
+    return acc + item.unitPrice * item.quantity - item.discount;
+  }, 0);
+
+  // create invoice
+  // const invoice = await
 };
